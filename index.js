87021ff4d4453bcde72d40ea5e841ddb81088c17
Jamie Gachie <jrgachie@ncsu.edu>
2020-04-28 18:39:09 -0400

    Added some console log messages for debugging purposes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3080
 
 const got = require('got');
 const http = require('http');
@@ -15,13 +15,15 @@ let TARGET = MASTER;
 let options = {};
 let proxy = httpProxy.createProxyServer(options);
 let server = http.createServer(function(req, res) {
-    if (req.url.includes('switch'))
+    if (req.url.includes('switch')) {
         TARGET = TARGET === MASTER ? BROKEN : MASTER;
+        console.log(`Switching server to ${TARGET}.`);
+    }
     else
         proxy.web(req, res, {target: TARGET});
 });
 
-server.listen(3080);
+server.listen(port, () => console.log(`Proxy service listening on 192.168.44.35:${port}`);
 
 
 
