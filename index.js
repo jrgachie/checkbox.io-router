4553a35bcdec048c704fe48130d8bf94cfff1eda
Jamie Gachie <jrgachie@ncsu.edu>
2020-04-28 18:25:08 -0400

    Rewrote checkbox.io-microservice-preview into a proxy server to redirect traffic between master/broken servers.

--- index.js
+++ index.js
@@ -2,18 +2,53 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const got = require('got');
+const http = require('http');
+const httpProxy = require('http-proxy');
+
 const marqdown = require('./marqdown');
 
-app.configure(function () {
-    app.use(express.bodyParser());
+const MASTER = 'http://192.168.44.25:3000/preview';
+const BROKEN = 'http://192.168.44.30:3000/preview';
+let TARGET = MASTER;
+
+let options = {};
+let proxy = httpProxy.createProxyServer(options);
+let server = http.createServer(function(req, res) {
+    if (req.url.includes('switch'))
+        TARGET = TARGET === MASTER ? BROKEN : MASTER;
+    else
+        proxy.web(req, res, {target: TARGET});
 });
 
+server.listen(3080);
 
-app.post('/preview', function(req,res)
-{
-    console.log(req.body.markdown);
-    var text = marqdown.render( req.body.markdown );
-    res.send( {preview: text} );
-})
 
-app.listen(port, () => console.log(`Microservice listening on http://localhost:${port}/preview`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.configure(function () {
+//     app.use(express.bodyParser());
+// });
+//
+//
+// app.post('/preview', function(req,res)
+// {
+//     console.log(req.body.markdown);
+//     var text = marqdown.render( req.body.markdown );
+//     res.send( {preview: text} );
+// })
+//
+// app.listen(port, () => console.log(`Microservice listening on http://localhost:${port}/preview`))
